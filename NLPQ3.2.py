ff06f381065479707186200ad670ceed6524a371
TinyNJ <50251632+TinyNJ@users.noreply.github.com>
2019-06-07 13:41:29 +0200

    Add files via upload

--- NLPQ3.2.py
+++ NLPQ3.2.py
@@ -31,3 +31,38 @@ def editDistance(str1, str2, m , n):
 str1 = "connect"
 str2 = "commute"
 print editDistance(str1, str2, len(str1), len(str2)) 
+
+###########Edit distance between connect and contact
+str1 = "connect"
+str2 = "contact"
+
+ 
+def editDistance(str1, str2, m , n): 
+  
+
+    if m==0: 
+         return n 
+  
+    # If second string is empty, the only option is to 
+    # remove all characters of first string 
+    if n==0: 
+        return m 
+  
+    # If last characters of two strings are same, nothing 
+    # much to do. Ignore last characters and get count for 
+    # remaining strings. 
+    if str1[m-1]==str2[n-1]: 
+        return editDistance(str1,str2,m-1,n-1) 
+  
+    # If last characters are not same, consider all three 
+    # operations on last character of first string, recursively 
+    # compute minimum cost for all three operations and take 
+    # minimum of three values. 
+    return 1 + min(editDistance(str1, str2, m, n-1),    # Insert 
+                   editDistance(str1, str2, m-1, n),    # Remove 
+                   editDistance(str1, str2, m-1, n-1)    # Substitute 
+                   )
+
+str1 = "connect"
+str2 = "contact"
+print editDistance(str1, str2, len(str1), len(str2)) 
